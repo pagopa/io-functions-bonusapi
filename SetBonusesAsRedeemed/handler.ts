5a905ffaed48a07e30e5808fe12001f05f73026f
Francesco Persico <francescopersico@gmail.com>
2020-06-09 09:20:46 +0200

    [#173217410] SetBonusesAsRedeemed skeleton (#3)
    * [#173217410] SetBonusesAsRedeemed skeleton
    
    * [#173217410] Use RequiredBodyPayloadMiddleware
    
    * [#173217410] Fix lint

---
+++ SetBonusesAsRedeemed/handler.ts
@@ -0,0 +1,44 @@
+import { Context } from "@azure/functions";
+import * as express from "express";
+
+import { ContextMiddleware } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+import { RequiredBodyPayloadMiddleware } from "io-functions-commons/dist/src/utils/middlewares/required_body_payload";
+import {
+  withRequestMiddlewares,
+  wrapRequestHandler
+} from "io-functions-commons/dist/src/utils/request_middleware";
+import {
+  IResponseSuccessAccepted,
+  ResponseSuccessAccepted
+} from "italia-ts-commons/lib/responses";
+
+import { RedeemedBonuses } from "../generated/definitions/RedeemedBonuses";
+import {
+  RequestAccepted,
+  StatusEnum
+} from "../generated/definitions/RequestAccepted";
+
+type ISetBonusesAsRedeemedHandler = (
+  context: Context,
+  redeemedBonuses: RedeemedBonuses
+) => Promise<IResponseSuccessAccepted<RequestAccepted>>;
+
+export function SetBonusesAsRedeemedHandler(): ISetBonusesAsRedeemedHandler {
+  return async (_, __) => {
+    return ResponseSuccessAccepted(undefined, {
+      status: StatusEnum.OK
+    });
+  };
+}
+
+export function SetBonusesAsRedeemed(): express.RequestHandler {
+  const handler = SetBonusesAsRedeemedHandler();
+
+  const middlewaresWrap = withRequestMiddlewares(
+    // Extract Azure Functions bindings
+    ContextMiddleware(),
+    RequiredBodyPayloadMiddleware(RedeemedBonuses)
+  );
+
+  return wrapRequestHandler(middlewaresWrap(handler));
+}
