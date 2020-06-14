726572beb44be34e79eae2d0b7a5a1f8ca5554b3
Francesco Persico <francescopersico@gmail.com>
2020-06-14 16:54:16 +0200

    [#173030678] Add RetrieveBonusActivity

--- ProcessRedeemedBonusOrchestrator/handler.ts
+++ ProcessRedeemedBonusOrchestrator/handler.ts
@@ -6,6 +6,11 @@ import { readableReport } from "italia-ts-commons/lib/reporters";
 import { IOrchestrationFunctionContext } from "durable-functions/lib/src/classes";
 
 import { RedeemedBonus } from "../generated/definitions/RedeemedBonus";
+import { BonusActivationStatusEnum } from "../generated/models/BonusActivationStatus";
+import {
+  ActivityInput as RetrieveBonusActivityInput,
+  ActivityResult as RetrieveBonusActivityResult
+} from "../RetrieveBonusActivity/handler";
 
 export const OrchestratorInput = RedeemedBonus;
 
@@ -29,7 +34,60 @@ export const handler = function*(
     return false;
   }
 
+  const redeemedBonus = errorOrOrchestratorInput.value;
+
   // STEP 1: Find the bonus in the db
+  const retrieveBonusActivityResultUnencoded = yield context.df.callActivity(
+    "RetrieveBonusActivity",
+    RetrieveBonusActivityInput.encode({
+      bonusID: redeemedBonus.bonus_code
+    })
+  );
+
+  const errorOrRetrieveBonusActivityResult = RetrieveBonusActivityResult.decode(
+    retrieveBonusActivityResultUnencoded
+  );
+
+  if (isLeft(errorOrRetrieveBonusActivityResult)) {
+    // Unrecoverable error, just log and return
+    context.log.error(
+      `${logPrefix}|Error decoding RetrieveBonusActivity result`
+    );
+    context.log.verbose(
+      `${logPrefix}|Error decoding RetrieveBonusActivity result|ERROR=${readableReport(
+        errorOrRetrieveBonusActivityResult.value
+      )}`
+    );
+    return false;
+  }
+
+  const retrieveBonusActivityResult = errorOrRetrieveBonusActivityResult.value;
+
+  if (retrieveBonusActivityResult.kind === "FAILURE") {
+    // Unrecoverable error, just log and return
+    context.log.error(
+      `${logPrefix}|Error in RetrieveBonusActivity|ERROR=${retrieveBonusActivityResult.reason}`
+    );
+    return false;
+  }
+
+  if (retrieveBonusActivityResult.kind === "SUCCESS_BONUS_NOT_FOUND") {
+    // The bonus doesn't exists
+    // TODO: Save the error somewhere to be analyzed
+    context.log.error(`${logPrefix}|The bonus doesn't exists`);
+    return false;
+  }
+
+  const bonus = retrieveBonusActivityResult.bonus;
+
+  if (bonus.status !== BonusActivationStatusEnum.ACTIVE) {
+    // We can't set as redeemed a bonus that is not active
+    // TODO: Save the error somewhere to be analyzed
+    context.log.error(
+      `${logPrefix}|The bonus is not active|STATUS=${bonus.status}`
+    );
+    return false;
+  }
 
   // STEP 2: Set the bonus status to redeemed
 
---
+++ __mocks__/models.ts
@@ -0,0 +1,132 @@
+import { IWithinRangeIntegerTag } from "italia-ts-commons/lib/numbers";
+import { FiscalCode, NonEmptyString } from "italia-ts-commons/lib/strings";
+import { BonusActivation } from "../generated/models/BonusActivation";
+import { BonusActivationStatusEnum } from "../generated/models/BonusActivationStatus";
+import { BonusCode } from "../generated/models/BonusCode";
+import { Dsu } from "../generated/models/Dsu";
+import {
+  EligibilityCheckFailure,
+  ErrorEnum as EligibilityCheckFailureErrorEnum,
+  StatusEnum as EligibilityCheckFailureStatusEnum
+} from "../generated/models/EligibilityCheckFailure";
+import {
+  EligibilityCheckSuccessEligible,
+  StatusEnum as EligibilityCheckSuccessEligibleStatus
+} from "../generated/models/EligibilityCheckSuccessEligible";
+import {
+  EligibilityCheckSuccessIneligible,
+  StatusEnum as EligibilityCheckSuccessIneligibleStatus
+} from "../generated/models/EligibilityCheckSuccessIneligible";
+
+import { MaxBonusAmount } from "../generated/models/MaxBonusAmount";
+import { MaxBonusTaxBenefit } from "../generated/models/MaxBonusTaxBenefit";
+import {
+  NewBonusActivation,
+  RetrievedBonusActivation
+} from "../models/bonus_activation";
+
+export const aFiscalCode = "AAABBB80A01C123D" as FiscalCode;
+
+export const aDsu: Dsu = {
+  dsuCreatedAt: new Date(),
+  dsuProtocolId: "aDsuProtocolId" as NonEmptyString,
+  familyMembers: [
+    {
+      fiscalCode: aFiscalCode,
+      name: "Mario" as NonEmptyString,
+      surname: "Rossi" as NonEmptyString
+    },
+    {
+      fiscalCode: "CCCDDD80A01C123D" as FiscalCode,
+      name: "Chiara" as NonEmptyString,
+      surname: "Bianchi" as NonEmptyString
+    }
+  ],
+  hasDiscrepancies: false,
+  iseeType: "iseeType",
+  maxAmount: 200 as MaxBonusAmount,
+  maxTaxBenefit: 80 as MaxBonusTaxBenefit,
+  requestId: "aRequestId" as NonEmptyString
+};
+
+export const aEligibilityCheckSuccessEligible: EligibilityCheckSuccessEligible = {
+  dsuRequest: aDsu,
+  id: (aFiscalCode as unknown) as NonEmptyString,
+  status: EligibilityCheckSuccessEligibleStatus.ELIGIBLE,
+  validBefore: new Date()
+};
+
+export const aEligibilityCheckSuccessEligibleValid: EligibilityCheckSuccessEligible = {
+  dsuRequest: aDsu,
+  id: (aFiscalCode as unknown) as NonEmptyString,
+  status: EligibilityCheckSuccessEligibleStatus.ELIGIBLE,
+  validBefore: new Date(Date.now() + 24 * 60 * 60 * 1000 /* +24h */)
+};
+export const aEligibilityCheckSuccessEligibleExpired: EligibilityCheckSuccessEligible = {
+  dsuRequest: aDsu,
+  id: (aFiscalCode as unknown) as NonEmptyString,
+  status: EligibilityCheckSuccessEligibleStatus.ELIGIBLE,
+  validBefore: new Date(Date.now() - 1 * 60 * 60 * 1000 /* -1h */)
+};
+
+export const aEligibilityCheckSuccessIneligible: EligibilityCheckSuccessIneligible = {
+  id: (aFiscalCode as unknown) as NonEmptyString,
+  status: EligibilityCheckSuccessIneligibleStatus.INELIGIBLE
+};
+
+export const aEligibilityCheckFailure: EligibilityCheckFailure = {
+  error: EligibilityCheckFailureErrorEnum.INTERNAL_ERROR,
+  errorDescription: "lorem ipsum",
+  id: (aFiscalCode as unknown) as NonEmptyString,
+  status: EligibilityCheckFailureStatusEnum.FAILURE
+};
+
+export const aBonusId = "AAAAAAAAAAAA" as NonEmptyString & BonusCode;
+export const aBonusActivationId = aBonusId;
+export const aBonusActivation: BonusActivation = {
+  id: "AAAAAAAAAAAA" as BonusCode,
+
+  applicantFiscalCode: aFiscalCode,
+
+  status: BonusActivationStatusEnum.ACTIVE,
+
+  createdAt: new Date(),
+
+  dsuRequest: {
+    familyMembers: [
+      {
+        fiscalCode: aFiscalCode,
+        name: "MARIO" as NonEmptyString,
+        surname: "ROSSI" as NonEmptyString
+      }
+    ],
+
+    maxAmount: (200 as unknown) as IWithinRangeIntegerTag<150, 501> & number,
+
+    maxTaxBenefit: (100 as unknown) as IWithinRangeIntegerTag<30, 101> & number,
+
+    requestId: "aRequestId" as NonEmptyString,
+
+    iseeType: "aISEEtype",
+
+    dsuProtocolId: "aProtocolId" as NonEmptyString,
+
+    dsuCreatedAt: new Date(),
+
+    hasDiscrepancies: false
+  }
+};
+
+export const aRetrievedBonusActivation: RetrievedBonusActivation = {
+  ...aBonusActivation,
+  _self: "xyz",
+  _ts: 123,
+  id: aBonusActivationId,
+  kind: "IRetrievedBonusActivation"
+};
+
+export const aNewBonusActivation: NewBonusActivation = {
+  ...aBonusActivation,
+  id: aBonusActivationId,
+  kind: "INewBonusActivation"
+};
