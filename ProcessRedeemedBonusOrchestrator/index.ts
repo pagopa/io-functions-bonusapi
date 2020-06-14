605efae9227f3246158607a904b0134c8446470d
Francesco Persico <francescopersico@gmail.com>
2020-06-14 15:45:15 +0200

    [#173030678] Add ProcessRedeemedBonusOrchestrator

---
+++ ProcessRedeemedBonusOrchestrator/index.ts
@@ -0,0 +1,6 @@
+﻿import * as df from "durable-functions";
+import { handler } from "./handler";
+
+const ProcessRedeemedBonusOrchestrator = df.orchestrator(handler);
+
+export default ProcessRedeemedBonusOrchestrator;
--- SetBonusesAsRedeemedOrchestrator/index.ts
+++
@@ -1,6 +0,0 @@
-﻿import * as df from "durable-functions";
-import { handler } from "./handler";
-
-const SetBonusesAsRedeemedOrchestrator = df.orchestrator(handler);
-
-export default SetBonusesAsRedeemedOrchestrator;
