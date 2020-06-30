2896a052f88581d9acdb72e9d53098b3a68dd88b
Loreau <laurabordin@gmail.com>
2020-06-30 17:37:14 +0200

    [#173557224] Review copy (#16)
    Copy aggiornato come da ultima versione del documento drive.

--- utils/messages.ts
+++ utils/messages.ts
@@ -8,11 +8,12 @@ export const getRedeemedBonusMessageContent = (
   const formattedDate = format(redeemedAt, "dd/MM/yyyy");
   return MessageContent.decode({
     markdown: `
-Il Bonus Vacanze concesso alla tua famiglia è stato speso il **${formattedDate}**.
+**Il Bonus Vacanze concesso alla tua famiglia è stato speso il ${formattedDate}**.
 
 Da questo momento in poi, lo troverai sempre nella sezione Pagamenti, con la dicitura ""Utilizzato"".
 
-Ti ricordiamo che la persona a cui è intestata la fattura del pagamento alla struttura ricettiva scelta, ha diritto a detrarre, nella prossima dichiarazione dei redditi, il 20% della somma pagata alla struttura ricettiva.
+Ti ricordiamo che l'utente a cui è intestata la fattura del pagamento alla struttura ricettiva scelta ha diritto a detrarre, nella prossima dichiarazione dei redditi, l'importo specificato nel dettaglio del Bonus Vacanze.
+Attenzione: se il prezzo pagato per la vacanza è inferiore all'importo massimo del bonus riconosciuto, la detrazione va ricalcolata: sarà pari al 20% della somma pagata alla struttura ricettiva
 `,
     subject: "Il tuo Bonus Vacanze è stato utilizzato con successo!"
   }).getOrElseL(() => {
