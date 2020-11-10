e168b62a2b9171a7547dd96bf09990fe3696120d
Simone <simone.biffi.91@gmail.com>
2020-11-10 10:39:19 +0100

    [#175488816] change bonus messages (#24)

--- utils/messages.ts
+++ utils/messages.ts
@@ -10,7 +10,7 @@ export const getRedeemedBonusMessageContent = (
     markdown: `
 **Il Bonus Vacanze concesso alla tua famiglia è stato speso il ${formattedDate}**.
 
-Da questo momento in poi, lo troverai sempre nella sezione Pagamenti, con la dicitura ""Utilizzato"".
+Da questo momento in poi, lo troverai sempre nella sezione Portafoglio, con la dicitura ""Utilizzato"".
 
 Ti ricordiamo che l'utente a cui è intestata la fattura del pagamento alla struttura ricettiva scelta ha diritto a detrarre, nella prossima dichiarazione dei redditi, l'importo specificato nel dettaglio del Bonus Vacanze.
 Attenzione: se il prezzo pagato per la vacanza è inferiore all'importo massimo del bonus riconosciuto, la detrazione va ricalcolata: sarà pari al 20% della somma pagata alla struttura ricettiva
